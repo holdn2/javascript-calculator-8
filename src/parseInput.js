ead6933c0b684fba2c06dd73af4c015799709af3
holdn2 <yoocy01@gmail.com>
2025-10-19 23:23:39 +0900

    refactor(parseInput): 입력값 검증 로직을 분리하고 커스텀 구분자 파싱 구조 개선

--- src/parseInput.js
+++ src/parseInput.js
@@ -1,47 +1,17 @@
-import { ERROR_MESSAGE, SEPERATOR } from './constants';
+import { EMPTY_STRING, REGEX, SEPERATOR } from './constants';
+import { validateRawInput } from './validator';
 
 export default function parseInput(input) {
-  if (
-    !input.includes(SEPERATOR.CUSTOM_START) ||
-    !input.includes(SEPERATOR.CUSTOM_END)
-  ) {
-    return { customSeperator: '', parsedInput: input };
+  validateRawInput(input);
+
+  if (input.startsWith(SEPERATOR.CUSTOM_START)) {
+    return { customSeperator: EMPTY_STRING, parsedInput: input };
   }
 
-  if (!input.startsWith(SEPERATOR.CUSTOM_START)) {
-    throw new Error(ERROR_MESSAGE.INVALID_CUSTOM_SEPERATOR_START);
-  }
-
-  const match = input.match(SEPERATOR.CUSTOM_REGEX);
-  if (!match) {
-    throw new Error(ERROR_MESSAGE.INVALID_CUSTOM_SEPERATOR_INDICATOR);
-  }
+  const match = input.match(REGEX.CUSTOM_INDICATOR);
 
   const customIndicator = match[0];
   const parsedInput = input.slice(customIndicator.length);
-  const customSeperator = getCustomSeperator(customIndicator);
 
-  return { customSeperator, parsedInput };
-}
-
-export function getCustomSeperator(customIndicator) {
-  const match = customIndicator.match(SEPERATOR.CUSTOM_REGEX);
-  if (!match) throw new Error(ERROR_MESSAGE.INVALID_CUSTOM_SEPERATOR_INDICATOR);
-
-  const customSeperator = match[1];
-
-  if (customSeperator.length !== 1) {
-    throw new Error(ERROR_MESSAGE.INVALID_SEPERATOR_LENGTH);
-  }
-  if (/^\d$/.test(customSeperator)) {
-    throw new Error(ERROR_MESSAGE.INVALID_CUSTOM_SEPERATOR_NUMBER);
-  }
-  if (
-    customSeperator === SEPERATOR.DEFAULT_COMMA ||
-    customSeperator === SEPERATOR.DEFAULT_COLON
-  ) {
-    throw new Error(ERROR_MESSAGE.INVALID_CUSTOM_SEPERATOR_DEFAULT);
-  }
-
-  return customSeperator;
+  return { customIndicator, parsedInput };
 }
