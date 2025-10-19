8d6325fc5c87706e3319eaecf2aa881ad604192e
holdn2 <yoocy01@gmail.com>
2025-10-19 23:37:03 +0900

    fix(parseInput): 오타 수정

--- src/parseInput.js
+++ src/parseInput.js
@@ -4,8 +4,8 @@ import { validateRawInput } from './validator';
 export default function parseInput(input) {
   validateRawInput(input);
 
-  if (input.startsWith(SEPERATOR.CUSTOM_START)) {
-    return { customSeperator: EMPTY_STRING, parsedInput: input };
+  if (!input.startsWith(SEPERATOR.CUSTOM_START)) {
+    return { customIndicator: EMPTY_STRING, parsedInput: input };
   }
 
   const match = input.match(REGEX.CUSTOM_INDICATOR);
