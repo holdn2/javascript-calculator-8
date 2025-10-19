404a982273121029ffe112191df763d9b3e6df47
holdn2 <yoocy01@gmail.com>
2025-10-20 02:27:02 +0900

    feat(App): calculator 계산 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+import { INFORM_MESSAGE } from './constants.js';
+import calculator from './calculator.js';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(INFORM_MESSAGE.INPUT);
+
+    const result = calculator(input);
+
+    Console.print(INFORM_MESSAGE.RESULT + result);
+  }
 }
 
 export default App;
