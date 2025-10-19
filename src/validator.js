79f0e8de209f951a1da9abb5c331dc59b4796723
holdn2 <yoocy01@gmail.com>
2025-10-19 21:43:34 +0900

    feat(validator): 커스텀 구분자에 대한 유효성 검사 추가

--- src/validator.js
+++ src/validator.js
@@ -22,3 +22,18 @@ function validateBoundaryNumbers(input) {
     throw new Error(ERROR_MESSAGE.INVALID_START_END_CHARACTER);
   }
 }
+
+export function validateCustomSeperator(customSeperator) {
+  if (customSeperator.length !== 1) {
+    throw new Error(ERROR_MESSAGE.INVALID_SEPERATOR_LENGTH);
+  }
+  if (REGEX.NUMBER.test(customSeperator)) {
+    throw new Error(ERROR_MESSAGE.INVALID_CUSTOM_SEPERATOR_NUMBER);
+  }
+  if (
+    customSeperator === SEPERATOR.DEFAULT_COMMA ||
+    customSeperator === SEPERATOR.DEFAULT_COLON
+  ) {
+    throw new Error(ERROR_MESSAGE.INVALID_CUSTOM_SEPERATOR_DEFAULT);
+  }
+}
