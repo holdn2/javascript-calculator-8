aa399d161a2234855d7e8d637a4c9a5b632e4749
holdn2 <yoocy01@gmail.com>
2025-10-20 01:41:55 +0900

    refactor(calculator): 구현한 함수 사용하여 리팩토링

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,13 @@
-// 숫자로 이루어진 문자 배열을 인수로 받아서 총합을 return 하는 함수
-export default function calculator(stringNumberArray) {
+import parseInput from './parseInput';
+import { extractCustomSeperator, extractNumArr } from './utils';
+
+export default function calculator(input) {
+  const { customIndicator, parsedInput } = parseInput(input);
+
+  const customSeperator = extractCustomSeperator(customIndicator);
+
+  const stringNumberArray = extractNumArr(customSeperator, parsedInput);
+
   let sum = 0;
   stringNumberArray.map((number) => {
     sum += Number(number);
