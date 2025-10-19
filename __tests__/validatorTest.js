e5f2b08c36a8f7da09fd00100e8a881337c68be8
holdn2 <yoocy01@gmail.com>
2025-10-19 23:10:31 +0900

    test(validatorTest): 커스텀 구분자, 파싱된 문자열 관련 테스트 추가

--- __tests__/validatorTest.js
+++ __tests__/validatorTest.js
@@ -1,5 +1,9 @@
 import { ERROR_MESSAGE } from '../src/constants';
-import { validateRawInput } from '../src/validator';
+import {
+  validateCustomSeperator,
+  validateParsedInput,
+  validateRawInput,
+} from '../src/validator';
 
 describe('validateRawInput', () => {
   test('커스텀 지시자가 하나도 포함되지 않은 경우, 앞뒤 글자가 숫자가 아니면 에러를 발생시킨다.', () => {
@@ -30,6 +34,103 @@ describe('validateRawInput', () => {
 
     expect(() => {
       validateRawInput(input);
+    }).not.toThrow();
+  });
+});
+
+describe('validateCustomSeperator', () => {
+  test('커스텀 구분자의 길이가 1보다 크다면 에러를 발생시킨다.', () => {
+    const customSeperator = '-.';
+
+    expect(() => {
+      validateCustomSeperator(customSeperator);
+    }).toThrow(ERROR_MESSAGE.INVALID_SEPERATOR_LENGTH);
+  });
+
+  test('커스텀 구분자 지시자 사이가 비어 있다면 에러를 발생시킨다.', () => {
+    const customSeperator = '';
+
+    expect(() => {
+      validateCustomSeperator(customSeperator);
+    }).toThrow(ERROR_MESSAGE.INVALID_SEPERATOR_LENGTH);
+  });
+
+  test('커스텀 구분자가 숫자라면 에러를 발생시킨다.', () => {
+    const customSeperator = '6';
+
+    expect(() => {
+      validateCustomSeperator(customSeperator);
+    }).toThrow(ERROR_MESSAGE.INVALID_CUSTOM_SEPERATOR_NUMBER);
+  });
+
+  test('커스텀 구분자가 기본 구분자(,)와 같다면 에러를 발생시킨다.', () => {
+    const customSeperator = ',';
+
+    expect(() => {
+      validateCustomSeperator(customSeperator);
+    }).toThrow(ERROR_MESSAGE.INVALID_CUSTOM_SEPERATOR_DEFAULT);
+  });
+
+  test('커스텀 구분자가 기본 구분자(:)와 같다면 에러를 발생시킨다.', () => {
+    const customSeperator = ':';
+
+    expect(() => {
+      validateCustomSeperator(customSeperator);
+    }).toThrow(ERROR_MESSAGE.INVALID_CUSTOM_SEPERATOR_DEFAULT);
+  });
+
+  test('커스텀 구분자가 숫자 또는 기본 구분자가 아니라면 통과한다.', () => {
+    const customSeperator = '.';
+
+    expect(() => {
+      validateCustomSeperator(customSeperator);
+    }).not.toThrow();
+  });
+});
+
+describe('validateParsedInput', () => {
+  test('파싱된 문자열의 맨 앞 숫자가 아니라면 에러를 발생시킨다.', () => {
+    const escapedSeperator = '=';
+    const parsedInput = ':5,2';
+
+    expect(() => {
+      validateParsedInput(escapedSeperator, parsedInput);
+    }).toThrow(ERROR_MESSAGE.INVALID_START_END_CHARACTER);
+  });
+
+  test('파싱된 문자열의 맨 뒤 숫자가 아니라면 에러를 발생시킨다.', () => {
+    const escapedSeperator = '=';
+    const parsedInput = '5,2=';
+
+    expect(() => {
+      validateParsedInput(escapedSeperator, parsedInput);
+    }).toThrow(ERROR_MESSAGE.INVALID_START_END_CHARACTER);
+  });
+
+  test('커스텀 구분자를 포함한 구분자 및 숫자가 아닌 문자가 있다면 에러를 발생시킨다.', () => {
+    const escapedSeperator = '=';
+    const parsedInput = '5,2=6!3';
+
+    expect(() => {
+      validateParsedInput(escapedSeperator, parsedInput);
+    }).toThrow(ERROR_MESSAGE.INVALID_CHARACTER);
+  });
+
+  test('구분자가 연속으로 오면 에러를 발생시킨다.', () => {
+    const escapedSeperator = '=';
+    const parsedInput = '5,2=,63';
+
+    expect(() => {
+      validateParsedInput(escapedSeperator, parsedInput);
+    }).toThrow(ERROR_MESSAGE.MISSING_NUMBER_BETWEEN_SEPERATORS);
+  });
+
+  test('맨 앞과 뒤가 숫자이며 숫자와 구분자로만 구성되고 구분자가 연속으로 오지 않으면 통과한다.', () => {
+    const escapedSeperator = '=';
+    const parsedInput = '5,2=63:3';
+
+    expect(() => {
+      validateParsedInput(escapedSeperator, parsedInput);
     }).not.toThrow();
   });
 });
