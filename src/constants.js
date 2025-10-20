f99e2c4c45b7ab7e5e29174f7ff6496af8ae3fc1
holdn2 <yoocy01@gmail.com>
2025-10-20 12:06:32 +0900

    refactor(constants): 에러 메시지 내의 문자열 표기 방식 통일

--- src/constants.js
+++ src/constants.js
@@ -5,14 +5,14 @@ export const INFORM_MESSAGE = {
 
 export const ERROR_MESSAGE = {
   INVALID_CHARACTER:
-    '[ERROR] 숫자와 구분자 이외의 다른 문자를 입력할 수 없습니다. (공백과 `.`도 일반 문자로 포함)',
+    '[ERROR] 숫자와 구분자 이외의 다른 문자를 입력할 수 없습니다. (공백과 "."도 일반 문자로 포함)',
   INVALID_CUSTOM_SEPARATOR_START:
-    '[ERROR] 커스텀 구분자 지시자인 // 는 문자열 맨 앞에 위치해야 합니다.',
+    '[ERROR] 커스텀 구분자 지시자인 "//" 는 문자열 맨 앞에 위치해야 합니다.',
   INVALID_SEPARATOR_LENGTH: '[ERROR] 커스텀 구분자는 한 글자여야 합니다.',
   INVALID_CUSTOM_SEPARATOR_NUMBER:
     '[ERROR] 커스텀 구분자로 숫자를 지정할 수 없습니다.',
   INVALID_CUSTOM_SEPARATOR_DEFAULT:
-    '[ERROR] ,과 :은 기본 구분자이므로 커스텀 구분자로 지정할 수 없습니다.',
+    '[ERROR] ","과 ":"은 기본 구분자이므로 커스텀 구분자로 지정할 수 없습니다.',
   INVALID_START_END_CHARACTER:
     '[ERROR] 문자열의 맨 앞과 뒤는 숫자여야 합니다. (커스텀 구분자를 지정하기 위한 문자열 제외)',
   MISSING_NUMBER_BETWEEN_SEPARATORS:
